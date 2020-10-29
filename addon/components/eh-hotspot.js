6f62e677342dd92f693d68ca450822d4b5cfd382
Florian Pichler <fp@ylk.gd>
2020-10-29 13:18:48 +0100

    Extend eh-hotspot implementation
    - Support for different roles (none/link/button)
    - Enable hover effects
    - Enable images in hotspots

--- addon/components/eh-hotspot.js
+++ addon/components/eh-hotspot.js
@@ -1,13 +1,21 @@
 import Component from '@glimmer/component';
 import { htmlSafe } from '@ember/template';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class EHHotspotComponent extends Component {
+  @service router;
+
   get backgroundImageInfo() {
     return window.EH_IMAGES[this.args.src] ?? {};
   }
 
   get imageMissing() {
     return this.backgroundImageInfo.width === undefined;
+  }
+
+  get isInteractive() {
+    return this.args.action || this.args.route;
   }
 
   get rect() {
@@ -24,13 +32,67 @@ export default class EHHotspotComponent extends Component {
   get style() {
     const { x, y, width, height } = this.rect;
 
-    const styles = [
-      `top:${y}px`,
-      `left:${x}px`,
-      `width:${width}px`,
-      `height:${height}px`,
-    ];
+    const styles = [`top:${y}px`, `left:${x}px`];
+
+    if (this.args.src) {
+      const {
+        width: imageWidth,
+        height: imageHeight,
+      } = this.backgroundImageInfo;
+
+      styles.push(
+        `width:${imageWidth}px`,
+        `height:${imageHeight}px`,
+        `background-image:url(${this.args.src})`
+      );
+    } else {
+      styles.push(`width:${width}px`, `height:${height}px`);
+    }
 
     return htmlSafe(styles.join(';'));
+  }
+
+  get triggerEvent() {
+    if (this.args.trigger === 'hover') {
+      return 'mouseenter'
+    }
+
+    if (this.args.trigger) {
+      return this.args.trigger;
+    }
+
+    return 'click';
+  }
+
+  @action
+  handleTrigger() {
+    this.args.action?.();
+
+    if (this.args.route) {
+      this.router.transitionTo(
+        this.args.route,
+        this.args.queryParams
+          ? {
+              queryParams: this.args.queryParams,
+            }
+          : {}
+      );
+    }
+  }
+
+  @action
+  onTrigger(event) {
+    this.handleTrigger();
+
+    if (this.args.trigger === 'hover') {
+      let callback = () => {
+        this.handleTrigger?.();
+        event.target.removeEventListener('mouseleave', callback);
+      }
+
+      event.target.addEventListener('mouseleave', callback)
+    }
+
+
   }
 }
